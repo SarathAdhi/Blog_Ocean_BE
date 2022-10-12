60e426f92012c525e4ffac5583f22bf6a387e25c
SarathAdhi <s.sarathadhithya@gmail.com>
2022-10-12 18:40:31 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { userRouter } from "./routes/user.routes";
+import { contentRouter } from "./routes/content.routes";
+
+dotenv.config();
+
+const app: Express = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.use("/user", userRouter);
+app.use("/content", contentRouter);
+
+mongoose.connect(process.env.MONGODB_URL!, () =>
+  console.log("Connected to MongoDB")
+);
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
