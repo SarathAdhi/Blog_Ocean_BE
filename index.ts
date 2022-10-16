5b46c7ebea47d2e26d079fa7404f0c76040d3f9b
SarathAdhi <s.sarathadhithya@gmail.com>
2022-10-16 16:59:05 +0530

    minor changes

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Blog ocean official API");
+  res.send({ message: "Blog ocean official API" });
 });
 
 app.use("/user", userRouter);
